ce5526a227e7f5603b75be19e11547fd0a44a3e3
Khale Coffran <khalecoffran@gmail.com>
2023-10-03 14:11:55 -0400

    bugs resolved

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,12 @@
+const preview = document.getElementById("preview-area");
+const source = document.getElementById("content-source");
+
 function renderHandler () {
-  let userIn = source.textContent
+  let userIn = source.value
 
   let userHTML = marked.parse(userIn)
 
   preview.innerHTML = userHTML
 }
 
-source.addEventListener('change', renderHandler)
+source.addEventListener('change', event => {renderHandler()});
